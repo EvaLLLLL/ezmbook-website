63ed8ea276c42ee22ad73270e9856016631f57ac
lyhhh <17610603221@163.com>
2020-09-22 15:18:06 +0800

    deploy

---
+++ precache-manifest.cf9389016c16830625838bb998c9616b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c5565a3183374a9e7e",
+    "url": "/ezmbook-website/css/app.44cd76f8.css"
+  },
+  {
+    "revision": "212dc43ac27f62b0c62599d6c8d084d8",
+    "url": "/ezmbook-website/index.html"
+  },
+  {
+    "revision": "d9c5565a3183374a9e7e",
+    "url": "/ezmbook-website/js/app.bfb12540.js"
+  },
+  {
+    "revision": "890d8b2264d7de528409",
+    "url": "/ezmbook-website/js/chunk-vendors.ec3144da.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/ezmbook-website/manifest.json"
+  },
+  {
+    "revision": "a410f8c7d6aa7eea45d739465bfc4291",
+    "url": "/ezmbook-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ezmbook-website/robots.txt"
+  }
+]);
